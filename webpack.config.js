f06037f13b04d1f570a4fcbcd30a7b3357f40889
Ligin Vellakkad <ligindd@gmail.com>
2017-10-24 15:27:58 +0530

    Add plugin for css file from scss.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 let config = {
     entry: './src/index.js', //entry file
@@ -12,10 +13,20 @@ let config = {
                 test: /\.js$/, // files ending with .js
                 exclude: /node_modules/, // exclude the node_modules directory
                 loader: "babel-loader" // use this (babel-core) loader
+            },
+            {
+                test: /\.scss$/, // files ending with .scss
+                use: ExtractTextWebpackPlugin.extract({
+                    use: ['css-loader', 'sass-loader'],
+                    fallback: 'style-loader'
+                }) //end extract
             },            
         ]
     },
-    plugins: [],
+    plugins: [
+    	new ExtractTextWebpackPlugin('style.css'),
+        //call the ExtractTextWebpackPlugin constructor and name our css file
+    ],
     devServer: {
         contentBase: path.resolve(__dirname, './public'),
         // A directory or URL to serve HTML content from.
